a3a680de2530a8621eef65f9f2ed16eb889c057f
Juda Alector <juda_alector@hotmail.com>
2019-11-24 19:59:42 -0600

    Añadir columnas para Disk

--- users/app/Http/Middleware/Cors.php
+++ users/app/Http/Middleware/Cors.php
@@ -15,8 +15,7 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        //header('Access-Control-Allow-Origin:  http://localhost:4200');
-        header('Access-Control-Allow-Origin:  *');
+        header('Access-Control-Allow-Origin: *');
         header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Authorization, Origin');
         header('Access-Control-Allow-Methods:  POST,GET,OPTIONS,PUT,PATCH,DELETE');
         return $next($request);
